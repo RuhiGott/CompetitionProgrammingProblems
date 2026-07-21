08c9f82f7cdad3c370f308fb982ca1b0e5f352d6
Ruhi Gottumukkala <126421448+RuhiGott@users.noreply.github.com>
2026-07-20 21:26:55 -0700

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       int[] letterCountArray = new int[26];
+
+       if (s.length() != t.length()) {
+            return false;
+       }
+
+        for (int i = 0; i < s.length(); i++) {
+            letterCountArray[s.charAt(i) - 'a']++;
+            letterCountArray[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < letterCountArray.length; i++) {
+            if (letterCountArray[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+       
+    }
+}
