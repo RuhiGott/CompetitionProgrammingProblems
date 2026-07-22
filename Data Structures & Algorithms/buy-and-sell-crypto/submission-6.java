3111370be6f462687fe6ba600b66ef56947f308c
Ruhi Gottumukkala <126421448+RuhiGott@users.noreply.github.com>
2026-07-21 19:55:03 -0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = 1;
+
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[right] < prices[left]) { 
+                left = right;
+            }
+            maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
