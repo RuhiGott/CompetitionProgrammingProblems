dc6a2ada2df735dc4470584e4a61386436fb0765
Ruhi Gottumukkala <126421448+RuhiGott@users.noreply.github.com>
2026-09-09 19:08:14 -0700

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adjList = new ArrayList[numCourses];
+
+        for (int[] pair : prerequisites) {
+            if (adjList[pair[1]] == null) {
+                adjList[pair[1]] = new ArrayList<>();
+            }
+
+            adjList[pair[1]].add(pair[0]);
+        }
+
+        int[] visited = new int[numCourses];
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0 && detectCycle(i, adjList, visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean detectCycle(int i, List<Integer>[] adjList, int[] visited) {
+        if (visited[i] == 1) {
+            return true;
+        } 
+
+        visited[i] = 1;
+
+        if (adjList[i] != null) {
+            for (Integer neighbor : adjList[i]) {
+               if (visited[neighbor] != 2) {
+                if (detectCycle(neighbor, adjList, visited)) {
+                    return true;
+                };
+               }
+            }
+            
+        }
+
+        visited[i] = 2;
+
+        return false;
+    }
+}
